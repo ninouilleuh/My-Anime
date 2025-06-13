3846a4c5b4f816e87c455b4a0bbac78a407dc96b
Palomares Nina <99027465+ninouilleuh@users.noreply.github.com>
2025-06-13 13:06:45 +0300

    anime airing

---
+++ animeData.js
@@ -0,0 +1,45 @@
+window.animeData = [
+    {
+        id: "frieren",
+        name: "Frieren: Beyond Journey’s End",
+        img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx154587-qQTzQnEJJ3oB.jpg",
+        genres: ["fantasy", "adventure", "drama"],
+        nextEpisodeDate: "2026-01-01T00:00:00Z"
+    },
+    {
+        id: "solo-leveling",
+        name: "Solo Leveling",
+        img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx151807-it355ZgzquUd.png",
+        genres: ["action", "fantasy", "adventure"]
+    },
+    {
+        id: "sword-art-online",
+        name: "Sword Art Online",
+        img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx11757-SxYDUzdr9rh2.jpg",
+        genres: ["action", "adventure", "fantasy", "sci-fi"]
+    },
+    {
+        id: "fullmetal-alchemist",
+        name: "Fullmetal Alchemist: Brotherhood",
+        img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx5114-nSWCgQlmOMtj.jpg",
+        genres: ["action", "adventure", "fantasy", "drama"]
+    },
+    {
+        id: "death-note",
+        name: "Death Note",
+        img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx1535-kUgkcrfOrkUM.jpg",
+        genres: ["drama", "mystery", "supernatural"]
+    },
+    {
+        id: "demon-slayer",
+        name: "Demon Slayer: Hashira Training Arc",
+        img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx101922-WBsBl0ClmgYL.jpg",
+        genres: ["action", "adventure", "fantasy"]
+    },
+    {
+        id: "kaiju-no-8",
+        name: "Kaiju No. 8",
+        img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx153288-25FBfFJzEQ5O.jpg",
+        genres: ["action", "sci-fi"]
+    }
+];
